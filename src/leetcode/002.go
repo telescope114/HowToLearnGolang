0650c612c6e92b6b81d8d9e98e46493fa1b66f5a
FCD郭瑞峰 <ruifeng.guo@fenrir-inc.com.cn>
2024-10-21 16:09:44 +0800

    feature: add leetcode text code

---
+++ src/leetcode/002.go
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var carry int
+	var head, tail *ListNode
+
+	for l1 != nil || l2 != nil || carry != 0 {
+		val1, val2 := int(0), int(0)
+
+		if l1 != nil {
+			val1 = l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			val2 = l2.Val
+			l2 = l2.Next
+		}
+
+		sum := val1 + val2 + carry
+		carry = sum / 10
+		newNode := &ListNode{Val: sum % 10}
+
+		if head == nil {
+			head = newNode
+			tail = newNode
+		} else {
+			tail.Next = newNode
+			tail = newNode
+		}
+	}
+
+	return head
+}
